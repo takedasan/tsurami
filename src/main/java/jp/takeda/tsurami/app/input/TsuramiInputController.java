27029a671a8c81cbe5ec1b3ff71a28634555ace2
takedasan <takedasan593@gmail.com>
2017-12-10 18:19:40 +0900

    Added first screen

---
+++ src/main/java/jp/takeda/tsurami/app/input/TsuramiInputController.java
@@ -0,0 +1,19 @@
+package jp.takeda.tsurami.app.input;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("input")
+public class TsuramiInputController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	String inputTsurami(Model model) {
+		model.addAttribute("tsurami", "つらみ");
+
+		return "input/inputTsurami";
+	}
+
+}
