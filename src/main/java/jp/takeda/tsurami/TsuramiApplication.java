768b388786decd12d3e3666bb136bb917097cc59
takedasan <takedasan593@gmail.com>
2017-12-10 17:59:49 +0900

    init commit

---
+++ src/main/java/jp/takeda/tsurami/TsuramiApplication.java
@@ -0,0 +1,12 @@
+package jp.takeda.tsurami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsuramiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsuramiApplication.class, args);
+	}
+}
